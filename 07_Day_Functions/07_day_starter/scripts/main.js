1ee07893dda2c4b7e4650194c16c56895530b34c
Bowie He <hbw234@outlook.com>
2022-08-23 22:49:01 +0800

    feat:finish task 7

--- 07_Day_Functions/07_day_starter/scripts/main.js
+++ 07_Day_Functions/07_day_starter/scripts/main.js
@@ -38,3 +38,58 @@ function solveQuadratic(a = 0, b = 0, c = 0) {
     return new Set([(-b + squ)/(2*a), (-b - squ)/(2*a)])
 }
 console.log(solveQuadratic(1, 4, 4))
+
+let showDateTime = function(){
+    var today = new Date()
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours()+':'+today.getMinutes()+':'+today.getSeconds();
+    console.log(date + ' ' + time)
+}
+
+showDateTime()
+
+function modifyArray(arr) {
+    if(arr.length < 5) {
+        return "Not Found"
+    }
+    let result = []
+    for(let i = 0; i < arr.length; i++) {
+        if(i != 4) {
+            result[i] = arr[i]
+        } else {
+            result[i] = String(arr[i]).toUpperCase()
+        }
+    }
+    return result
+}
+
+console.log(modifyArray(['Avocado', 'Tomato', 'Potato','Mango', 'Lemon','Carrot']))
+
+function isUnique(arr) {
+    let result = []
+    for(const elem of arr) {
+        if(result.includes(elem)) {
+            return false
+        }else {
+            result.push(elem)
+        }
+    }
+    return true
+}
+
+console.log(isUnique([1,2,3,4]))
+console.log(isUnique([1,1]))
+
+function isUniqueType(arr) {
+    let type = arr[0].constructor.name
+    // console.log(type)
+    for(const elem of arr) {
+        if(elem.constructor.name != type) {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(isUniqueType([1,2,4]))
+console.log(isUniqueType([1, "asd"]))

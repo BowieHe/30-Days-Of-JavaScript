0adc1020bb19911f87e38c513004fab8e0475703
Bowie He <hbw234@outlook.com>
2022-09-05 21:48:53 +0800

    feat:add 09 functions

--- 08_Day_Objects/08_day_starter/scripts/main.js
+++ 08_Day_Objects/08_day_starter/scripts/main.js
@@ -2,66 +2,88 @@
 // alert('Open the console and check if the countries has been loaded')
 
 const users = {
-    Alex: {
-      email: 'alex@alex.com',
-      skills: ['HTML', 'CSS', 'JavaScript'],
-      age: 20,
-      isLoggedIn: false,
-      points: 30
-    },
-    Asab: {
-      email: 'asab@asab.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
-      age: 25,
-      isLoggedIn: false,
-      points: 50
-    },
-    Brook: {
-      email: 'daniel@daniel.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
-      age: 30,
-      isLoggedIn: true,
-      points: 50
-    },
-    Daniel: {
-      email: 'daniel@alex.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
-      age: 20,
-      isLoggedIn: false,
-      points: 40
-    },
-    John: {
-      email: 'john@john.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
-      age: 20,
-      isLoggedIn: true,
-      points: 50
-    },
-    Thomas: {
-      email: 'thomas@thomas.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'React'],
-      age: 20,
-      isLoggedIn: false,
-      points: 40
-    },
-    Paul: {
-      email: 'paul@paul.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
-      age: 20,
-      isLoggedIn: false,
-      points: 40
-    }
-
+  Alex: {
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    age: 20,
+    isLoggedIn: false,
+    points: 30
+  },
+  Asab: {
+    email: 'asab@asab.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 25,
+    isLoggedIn: false,
+    points: 50
+  },
+  Brook: {
+    email: 'daniel@daniel.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+    age: 30,
+    isLoggedIn: true,
+    points: 50
+  },
+  Daniel: {
+    email: 'daniel@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  John: {
+    email: 'john@john.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+    age: 20,
+    isLoggedIn: true,
+    points: 50
+  },
+  Thomas: {
+    email: 'thomas@thomas.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  Paul: {
+    email: 'paul@paul.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
   }
 
-function  getInfo(name) {
-    return users[name]
+}
+
+function isMERN() {
+
+  var mern = ["MongoDB", "Express", "React", "Node"]
+  var isFullStack = false
+  var user = []
+  for (const person of Object.keys(user)) {
+    let skills = getInfo(person).skills
+    for (const sk of mern) {
+      if (!skills.includes(sk)) {
+        isFullStack = false
+        break
+      }
+    }
+    if (isFullStack) {
+      user.push(user.email)
+    }
+  }
+  return user
+}
+
+
+
+function getInfo(name) {
+  return users[name]
 }
 
 function getSkillNum(name) {
-    let info = Object.assign({}, getInfo(name))
-    
-    return info.skills.length
+  let info = Object.assign({}, getInfo(name))
+
+  return info.skills.length
 }
 
 let names = Object.keys(users)
@@ -73,27 +95,28 @@ const mernStack = []
 const stackNames = []
 const mern = ["MongoDB", "Express", "React", "Node"]
 
-for(let i = 0; i < names.length; i++) {
-    let userInfo = getInfo(names[i])
-    // console.log(userInfo)
+for (let i = 0; i < names.length; i++) {
+  let userInfo = getInfo(names[i])
+  // console.log(userInfo)
+  // console.log(userInfo.skills)
 
-    // console.log(Object.values(userName))
-    // console.log(student)
-    // let skillNum = getSkillNum(names[i])
-    // console.log(skillNum)
+  // console.log(Object.values(userName))
+  // console.log(student)
+  // let skillNum = getSkillNum(names[i])
+  // console.log(skillNum)
 
-    let isStack = true
-    let skills = Object.values(userInfo.skills)
-    for(const skill of mern) {
-      if(!skills.includes(skill)) {
-        isStack = false
-        break
-      }
+  let isStack = true
+  // let skills = userInfo.skills
+  for (const skill of mern) {
+    if (!userInfo.skills.includes(skill)) {
+      isStack = false
+      break
     }
-    if(isStack) {
-      mernStack.push(userInfo)
-      stackNames.push(names[i])
-    }
+  }
+  if (isStack) {
+    mernStack.push(userInfo)
+    stackNames.push(names[i])
+  }
 }
 
 console.log(mernStack)
@@ -104,3 +127,5 @@ console.log(Object.entries(mernStack[0]))
 console.log(Object.values(stackNames))
 
 alert(`People ${stackNames.join(",")} is MERN stack developer`)
+
+console.log(isMERN())
--- 09_Day_Higher_order_functions/09_day_starter/scripts/main.js
+++ 09_Day_Higher_order_functions/09_day_starter/scripts/main.js
@@ -2,31 +2,83 @@
 
 // alert('Open the console and check if the countries has been loaded')
 
- const callback = (n) => {
+const callback = (n) => {
     return n ** 2
- }
- function cube(callback, n) {
-    return callback(n) * n
- }
-
- console.log(cube(callback, 3))
-
- const numbers = [1,2,3,4,5]
- const sumArray = arr => {
-    let sum = 0
-    arr.forEach(element => {
-        sum += element
-    });
-    return sum
 }
-console.log(sumArray(numbers))
+function cube(callback, n) {
+    return callback(n) * n
+}
+
+console.log(cube(callback, 3))
 
 function printIntervalEverySecond() {
     console.log(new Date().getDay())
 }
-setInterval(printIntervalEverySecond, 1000)
+// setInterval(printIntervalEverySecond, 1000)
 
 function printInfuture() {
     console.log('this is future')
 }
-setTimeout(printInfuture, 1000)
+
+// setTimeout(printInfuture, 1000)
+const country = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const products = [
+    { product: 'banana', price: 3 },
+    { product: 'mango', price: 6 },
+    { product: 'potato', price: ' ' },
+    { product: 'avocado', price: 8 },
+    { product: 'coffee', price: 10 },
+    { product: 'tea', price: '' },
+]
+
+let sortedCountry = country.sort(function(a, b) {
+    if(a.name > b.name) {
+        return 1
+    } else if(a.capital > b.capital) {
+        return 1
+    } else if(a.population > b.population) {
+        return 1
+    } else {
+        return -1
+    }
+})
+console.log(sortedCountry)
+
+const county1 = [
+{country: 'English',count:91},
+{country: 'French',count:45},
+{country: 'Arabic',count:25},
+{country: 'Spanish',count:24},
+{country:'Russian',count:9},
+{country:'Portuguese', count:9},
+{country:'Dutch',count:8},
+{country:'German',count:7},
+{country:'Chinese',count:5},
+{country:'Swahili',count:4}
+]
+
+function mostSpokenLanguages(countries, num) {
+    var res = []
+    
+}
+
+var allLanguages = []
+countries.forEach(country => 
+    country.languages.forEach(lan => 
+        allLanguages.push(lan)))
+var prev = allLanguages[0]
+var count = 0
+let res = []
+for(lang of allLanguages) {
+    if(lang == prev) {
+        count++
+    } else {
+        res.push({language: lang, count: count})
+        prev = lang
+        count = 1
+    }
+}
+res.push({language:prev, count: count})
+console.log(res.sort(function(a, b) {a.count - b.count}))

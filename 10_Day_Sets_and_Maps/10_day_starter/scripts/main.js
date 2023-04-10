b579e91c9d24b2cbc983a540fff983e02b1ed9d7
Bowie He <hbw234@outlook.com>
2023-04-10 22:04:06 +0800

    add note

--- 10_Day_Sets_and_Maps/10_day_starter/scripts/main.js
+++ 10_Day_Sets_and_Maps/10_day_starter/scripts/main.js
@@ -1,2 +1,51 @@
 console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// alert('Open the console and check if the countries has been loaded')
+
+console.log("test")
+const language = [
+    'English',
+    'Finnish',
+    'English',
+    'French',
+    'Spanish',
+    'English',
+    'French',
+]
+const setOfLanguages = new Set(language)
+console.log(setOfLanguages)
+
+// setOfLanguages.clear()
+
+console.log(setOfLanguages)
+
+const counts = []
+const count = {}
+
+for(const l of setOfLanguages) {
+    const filterLang = language.filter((lan) => lan == l)
+    // console.log(filterLang)
+    counts.push({lang: l, count: filterLang.length}) 
+}
+
+console.log(counts)
+
+const a = [4, 5, 8, 9]
+const b = [3, 4, 5, 7]
+// const countries = ['Finland', 'Sweden', 'Norway']
+
+const empty = new Set()
+for(let i = 0; i < 10; i++) {
+    empty.add(i)
+}
+
+console.log(empty)
+
+empty.delete(1)
+console.log(empty)
+empty.clear()
+
+const countryMap = new Map()
+for(let count of countries) {
+    countryMap.set(count.name, count.name.length)
+}
+console.log(countryMap)

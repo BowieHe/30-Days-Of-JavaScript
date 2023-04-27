4ff0822d71cc0aa810965b904c2faaefc4e94fc5
Bowie He <hbw234@outlook.com>
2023-04-27 08:13:16 +0800

    add result

--- 13_Day_Console_object_methods/13_day_starter/scripts/main.js
+++ 13_Day_Console_object_methods/13_day_starter/scripts/main.js
@@ -1,2 +1,17 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// console.log(countries)
+// alert('Open the console and check if the countries has been loaded')
+
+
+console.log('%c30 Days Of JavaScript', 'color:green') // log output is green
+console.log(
+  '%c30 Days%c %cOf%c %cJavaScr%cipt',
+  'color:green',
+  '',
+  'color:red',
+  '',
+  'color:yellow',
+  'color:grey'
+) // log output green red and yellow text
+
+const names = ['Asabeneh', 'Brook', 'David', 'John']
+console.table(names)

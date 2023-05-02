4709432f8d9e8a5b77ba8b0e899887dbf07accbf
Bowie He <hbw234@outlook.com>
2023-05-02 13:36:53 +0800

    add result

--- 13_Day_Console_object_methods/13_day_starter/scripts/main.js
+++ 13_Day_Console_object_methods/13_day_starter/scripts/main.js
@@ -1,5 +1,5 @@
-// console.log(countries)
-// alert('Open the console and check if the countries has been loaded')
+// console.log(county)
+// alert('Open the console and check if the county has been loaded')
 
 
 console.log('%c30 Days Of JavaScript', 'color:green') // log output is green
@@ -13,5 +13,88 @@ console.log(
   'color:grey'
 ) // log output green red and yellow text
 
+// const names = ['Asabeneh', 'Brook', 'David', 'John']
+// console.table(names)
+
+const u = {
+  name: "test"
+}
+console.table(u)
+
 const names = ['Asabeneh', 'Brook', 'David', 'John']
-console.table(names)
+const county = [
+  ['Finland', 'Helsinki'],
+  ['Sweden', 'Stockholm'],
+  ['Norway', 'Oslo']
+]
+const user = {
+  name: 'Asabeneh',
+  title: 'Programmer',
+  country: 'Finland',
+  city: 'Helsinki',
+  age: 250
+}
+const users = [
+  {
+    name: 'Asabeneh',
+    title: 'Programmer',
+    country: 'Finland',
+    city: 'Helsinki',
+    age: 250
+  },
+  {
+    name: 'Eyob',
+    title: 'Teacher',
+    country: 'Sweden',
+    city: 'London',
+    age: 25
+  },
+  {
+    name: 'Asab',
+    title: 'Instructor',
+    country: 'Norway',
+    city: 'Oslo',
+    age: 22
+  },
+  {
+    name: 'Matias',
+    title: 'Developer',
+    country: 'Denmark',
+    city: 'Copenhagen',
+    age: 28
+  }
+]
+
+console.group('Names')
+console.log(names)
+console.groupEnd()
+
+console.group('county')
+console.log(county)
+console.groupEnd()
+
+console.group('Users')
+console.log(user)
+console.log(users)
+console.groupEnd()
+
+
+console.assert(10 > Math.pow(2, 10), 'this is error')
+
+console.warn("this is a warning ")
+
+console.error("this is an error")
+
+console.time("for")
+for(let i = 1; i <= 10; i++) {
+  // do nothing
+}
+console.timeEnd("for")
+
+console.time("while") 
+let j = 0
+while(j < 10){
+  // do nothing
+  j++
+}
+console.timeEnd("while")

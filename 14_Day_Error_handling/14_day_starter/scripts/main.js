468e112b3929e21f18dce8059cd603cf84537d41
Bowie He <hbw234@outlook.com>
2023-05-02 14:06:33 +0800

    add result

--- 14_Day_Error_handling/14_day_starter/scripts/main.js
+++ 14_Day_Error_handling/14_day_starter/scripts/main.js
@@ -1,2 +1,8 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// console.log(countries)
+// alert('Open the console and check if the countries has been loaded')
+
+try {
+    let a = v + false
+} catch(err) {
+    console.error(err)
+}

e0447ce2f8e394c5b87d0e296683b3d3d31e35f4
Bowie He <hbw234@outlook.com>
2023-05-11 21:25:28 +0800

    add note

--- 18_Day_Promises/18_day_starter/scripts/main.js
+++ 18_Day_Promises/18_day_starter/scripts/main.js
@@ -3,46 +3,46 @@
 
 const doSomething = callback => {
     setTimeout(() => {
-      const skills = ['HTML', 'CSS', 'JS']
-      callback('It did not go well', skills)
+        const skills = ['HTML', 'CSS', 'JS']
+        callback('It did not go well', skills)
     }, 2000)
-  }
-  
-  const callback = (err, result) => {
+}
+
+const callback = (err, result) => {
     if (err) {
-      return console.log(err)
+        return console.log(err)
     }
     return console.log(result)
-  }
-  
-  doSomething(callback)
+}
+
+doSomething(callback)
 
 
-  // Promise
+// Promise
 const doPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      const skills = ['HTML', 'CSS', 'JS']
-      if (skills.includes('Node')) {
-        resolve('fullstack developer')
-      } else {
-        reject('Something wrong has happened')
-      }
+        const skills = ['HTML', 'CSS', 'JS']
+        if (skills.includes('Node')) {
+            resolve('fullstack developer')
+        } else {
+            reject('Something wrong has happened')
+        }
     }, 2000)
-  })
-  
-  doPromise
+})
+
+doPromise
     .then(result => {
-      console.log(result)
+        console.log(result)
     })
     .catch(error => console.error(error))
 
 
 
-    const url = 'https://restcountries.com/v2/all' // countries api
-    fetch(url)
-      .then(response => response.json()) // accessing the API data as JSON
-      .then(data => {
+const url = 'https://restcountries.com/v2/all' // countries api
+fetch(url)
+    .then(response => response.json()) // accessing the API data as JSON
+    .then(data => {
         // getting the data
         console.log(data)
-      })
-      .catch(error => console.error(error)) // handling error if something wrong happens
+    })
+    .catch(error => console.error(error)) // handling error if something wrong happens
--- 19_Day_Closures/19_day_starter/scripts/main.js
+++ 19_Day_Closures/19_day_starter/scripts/main.js
@@ -1,2 +1,63 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// console.log(countries)
+// alert('Open the console and check if the countries has been loaded')
+
+function outer() {
+    let count = 0
+    function plus() {
+        count++
+        return count
+    }
+
+    function minus() {
+        count--
+        return count
+    }
+
+    function equal() {
+        console.log("test")
+        count *= count
+    }
+    return {
+        pl: plus(),
+        mi: minus(),
+        e: equal()
+    }
+}
+
+const inner = outer()
+console.log(inner.mi)
+console.log(inner.e)
+console.log(inner.pl)
+
+function personAccount(num) {
+    let firstname = 'a'
+    let lastname = 'b'
+    let incomes = 100
+    let expenses = 3
+
+    function totalincome() {
+        return incomes - expenses
+    }
+
+    function totalExpense() {
+        return expenses
+    }
+
+    function accountInfo() {
+        return firstname + ' ' + lastname
+    }
+
+    function addIncome() {
+        return incomes += num
+    }
+
+    return {
+        totalincome: totalincome(),
+        totalExpense: totalExpense(),
+        accountInfo: accountInfo(),
+        addIncome: addIncome()
+    }
+}
+
+const person = personAccount(23)
+console.log(person.addIncome)
